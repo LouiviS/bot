43dbf0f6c0960b21a3a56de345f6e5c8dd42f2db
LouiviS <73073436+LouiviS@users.noreply.github.com>
2020-10-20 09:42:39 +0200

    Add files via upload

--- commands/say.lua
+++ commands/say.lua
@@ -1,13 +1,36 @@
-local Struct = {}
-Struct.Name = "-say"
-Struct.Aliases = {
-    '-dit',
-    '-dis',
-    '-di'
-}
-
+local Struct = {
+    ['Name'] = "-say",
+    ['Aliases'] = {
+        '-dit',
+        '-dis',
+        '-di'},}   
 Struct.Execute = function(Client, Message, Args)
-    Message:reply("Commande Say")
+    if not Args[2] then
+        return Message:reply {
+            embed = {
+                title = "Erreur !!",
+                description = 'Veuillez inscrire le message à envoyer.\nExemple: -say Salut tout le monde',
+                footer = {
+                    text = 'Demandé par ' .. Message.author.tag
+                },
+                color = 0xff0000
+            }}
+    end
+
+    -- [[ Delete Args[1] ]]
+    table.remove(Args, 1)
+    local Content = table.concat(Args, " ")
+    -- [[ Delete message ]]
+    Message:delete()
+    -- [[ Send required message as embed ]]
+    return Message:reply {
+        embed = {
+            description = Content,
+            footer = {
+                text = "Envoyé par " .. Message.author.tag
+            },
+            color = 0x1ba8ce
+        }}
 end
 
 return Struct
